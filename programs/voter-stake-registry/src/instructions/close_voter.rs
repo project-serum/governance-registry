9c376c2a5683697664c4036271cc12fcd165fee9
microwavedcola1 <microwavedcola@gmail.com>
2022-04-08 14:55:33 +0200

    zero out voter completely
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- programs/voter-stake-registry/src/instructions/close_voter.rs
+++ programs/voter-stake-registry/src/instructions/close_voter.rs
@@ -1,7 +1,10 @@
+use std::ops::DerefMut;
+
 use crate::error::*;
 use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, CloseAccount, Token, TokenAccount};
+use bytemuck::bytes_of_mut;
 
 // Remaining accounts must be all the token token accounts owned by voter, he wants to close,
 // they should be writable so that they can be closed and sol required for rent
@@ -17,7 +20,8 @@ pub struct CloseVoter<'info> {
         seeds = [voter.load()?.registrar.key().as_ref(), b"voter".as_ref(), voter_authority.key().as_ref()],
         bump = voter.load()?.voter_bump,
         has_one = voter_authority,
-        close = sol_destination)]
+        close = sol_destination
+    )]
     pub voter: AccountLoader<'info, Voter>,
 
     pub voter_authority: Signer<'info>,
@@ -34,31 +38,42 @@ pub struct CloseVoter<'info> {
 pub fn close_voter<'key, 'accounts, 'remaining, 'info>(
     ctx: Context<'key, 'accounts, 'remaining, 'info, CloseVoter<'info>>,
 ) -> Result<()> {
-    let voter = &ctx.accounts.voter.load()?;
-    let amount = voter.deposits.iter().fold(0u64, |sum, d| {
-        sum.checked_add(d.amount_deposited_native).unwrap()
-    });
-    require!(amount == 0, VotingTokenNonZero);
+    {
+        let voter = ctx.accounts.voter.load()?;
+        let amount = voter.deposits.iter().fold(0u64, |sum, d| {
+            sum.checked_add(d.amount_deposited_native).unwrap()
+        });
+        require!(amount == 0, VotingTokenNonZero);
 
-    let voter_seeds = voter_seeds!(voter);
-    for account in &mut ctx.remaining_accounts.iter() {
-        let token = Account::<TokenAccount>::try_from(&account.clone()).unwrap();
-        require!(token.owner == ctx.accounts.voter.key(), InvalidAuthority);
-        require!(token.amount == 0, VaultTokenNonZero);
+        let voter_seeds = voter_seeds!(voter);
+        for account in &mut ctx.remaining_accounts.iter() {
+            let token = Account::<TokenAccount>::try_from(&account.clone()).unwrap();
+            require!(token.owner == ctx.accounts.voter.key(), InvalidAuthority);
+            require!(token.amount == 0, VaultTokenNonZero);
 
-        let cpi_accounts = CloseAccount {
-            account: account.to_account_info(),
-            destination: ctx.accounts.sol_destination.to_account_info(),
-            authority: ctx.accounts.voter.to_account_info(),
-        };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        token::close_account(CpiContext::new_with_signer(
-            cpi_program,
-            cpi_accounts,
-            &[voter_seeds],
-        ))?;
+            let cpi_accounts = CloseAccount {
+                account: account.to_account_info(),
+                destination: ctx.accounts.sol_destination.to_account_info(),
+                authority: ctx.accounts.voter.to_account_info(),
+            };
+            let cpi_program = ctx.accounts.token_program.to_account_info();
+            token::close_account(CpiContext::new_with_signer(
+                cpi_program,
+                cpi_accounts,
+                &[voter_seeds],
+            ))?;
 
-        account.exit(ctx.program_id)?;
+            account.exit(ctx.program_id)?;
+        }
+    }
+
+    // zero out voter account to prevent reinit attacks
+    // appease rust borrow checker
+    {
+        let mut voter = ctx.accounts.voter.load_mut()?;
+        let voter_dereffed = voter.deref_mut();
+        let voter_bytes = bytes_of_mut(voter_dereffed);
+        voter_bytes.fill(0);
     }
 
     Ok(())
